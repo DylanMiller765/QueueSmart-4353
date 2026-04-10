e1b0165c60079d4bcb03cffee07b6da42786e255
Stuti Pagadala <pagadalastuti@gmail.com>
2026-04-09 22:28:54 -0500

    Fixing the env variable

--- queuesmart/jest.setup.ts
+++ queuesmart/jest.setup.ts
@@ -1,3 +1,2 @@
-process.env.NEXT_PUBLIC_SUPABASE_URL = "https://rxdxlspyipjsrgsttvbi.supabase.co/";
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4ZHhsc3B5aXBqc3Jnc3R0dmJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU2ODczMzMsImV4cCI6MjA5MTI2MzMzM30.snasElHAObtIsfIH5E9yCau3RrkZYss4NR2WTV-Cijk";
-process.env.SUPABASE_SERVICE_ROLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4ZHhsc3B5aXBqc3Jnc3R0dmJpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTY4NzMzMywiZXhwIjoyMDkxMjYzMzMzfQ.pG1DzZKUp9f6yvsvCbrj3co1pD6785hs_m56ydHY384";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
